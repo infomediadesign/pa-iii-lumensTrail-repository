9ec4b7e5e884d6782445478e2a1bebb5ee4bf0b2
LordBrandon333 <148345020+LordBrandon333@users.noreply.github.com>
2024-11-12 18:11:32 +0100

    Updated PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,36 +5,57 @@ public class PlayerController : MonoBehaviour
 {
 
     private PlayerInput playerInput;
+    private StateMachine playerStateMachine;
+    private CharacterController characterController;
 
-    private InputAction jumpAction;
-    private InputAction attackAction;
+    public bool isGrounded = false;
+    private bool isTouchingWall = false;
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
-        jumpAction = playerInput.actions["Jump"];
-        attackAction = playerInput.actions["Attack"];
+        characterController = GetComponent<CharacterController>();
+        playerStateMachine = GetComponent<StateMachine>();
     }
 
-    private void OnEnable()
+    private void Update()
     {
-        jumpAction.performed += OnJump;
-        attackAction.performed += OnAttack;
+        isGrounded = CheckGround();
+        isTouchingWall = CheckWall();
     }
 
-    private void OnDisable()
+    private bool CheckGround()
     {
-        jumpAction.performed -= OnJump;
-        attackAction.performed -= OnAttack;
+        // Raycast to check for collision underneath the player
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down, 5f);
+        if (hit.collider.CompareTag("Ground")) Debug.Log("Hit");
+        // First: check if Raycast hit an object, second: check if object has Tag "Ground"
+        return hit.collider != null && hit.collider.CompareTag("Ground");
     }
 
-    private void OnJump(InputAction.CallbackContext context)
+    private bool CheckWall()
     {
-        Debug.Log("Jumping");
+        // Raycast to check for collision left and right of the player
+        RaycastHit2D hitLeft = Physics2D.Raycast(transform.position, Vector2.left, 0.3f);
+        RaycastHit2D hitRight = Physics2D.Raycast(transform.position, Vector2.right, 0.3f);
+        // For both sides, check if Raycast hit something and check if object has tag "Wall"
+        return (hitLeft.collider != null && hitLeft.collider.CompareTag("Wall")) || (hitRight.collider != null && hitRight.collider.CompareTag("Wall"));
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Jump")
+        }
+        
     }
 
     private void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Attacking");
+        if (context.performed)
+        {
+            Debug.Log("Attack");
+        }
     }
 }
